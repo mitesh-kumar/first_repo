aefa723e0df6b470e0b7beebc97ab65ddbb3fb68
mitesh kumar <mtshkmr8@gmail.com>
2014-08-22 11:18:10 +0530

    depote adding

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,4 @@
+#encoding: utf-8
+
+I18n.default_locale = :en
+LANGUAGES = [['English',          'en'], ["Espan&ntilde;ol".html_safe, 'hi']]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'f86f11f204f344e0fd4aa6b71ff307cb42350c71be4f4acbec4e7690910884da62ce9667ab039ea8bd28d3a49267a783ec7368e295ea6304943edb0dd182cc1d'
